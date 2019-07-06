62957307dd513343dfbabb37af69f8d0bd908746
Meet Patel <45942161+Meet57@users.noreply.github.com>
2019-07-07 03:08:10 +0530

    sorting array operation

--- DSA/array operations/main.cpp
+++ DSA/array operations/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Data
 {
 protected:
-    int a[100],n,i,temp,top=(-1);
+    int a[100],n,i,j,temp,top=(-1);
 public:
     void Display()
     {
@@ -12,6 +12,52 @@ public:
         for(i=0;i<=top;i++)
         {
             cout << a[i] << endl;
+        }
+    }
+    void rsort()
+    {
+        if(top>(-1))
+        {
+            for(i=top; i>=0; i--)
+            {
+                for(j=i-1;j>=0;j--)
+                {
+                    if(a[j] < a[i])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            cout << "Array sorted " << endl;
+        }
+        else
+        {
+            cout << "Array underflow" << endl;
+        }
+    }
+    void sort()
+    {
+        if(top>(-1))
+        {
+            for(i=0; i<=top; i++)
+            {
+                for(j=i+1; j<=top; j++)
+                {
+                    if(a[j] < a[i])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            cout << "Array sorted " << endl;
+        }
+        else
+        {
+            cout << "Array underflow" << endl;
         }
     }
     //Insert
@@ -81,6 +127,29 @@ public:
             cout << "Position doesn't exist" << endl;
         }
     }
+    void deletespecific()
+    {
+        cout << "Enter the value you want to delete : ";
+        cin>>temp;
+
+        n=0;
+        for(i=0;i<=top;i++)
+        {
+            if(a[i]==temp)
+            {
+                for(j=i;j<=top;j++)
+                {
+                    a[j] = a[j+1];
+                    n = n+1;
+                    top = top - 1;
+                }
+            }
+        }
+        if(n==0)
+        {
+            cout << "The value Doesn't exist" << endl;
+        }
+    }
 };
 
 
@@ -97,6 +166,9 @@ main()
         cout << "2. Delete element at last" << endl;
         cout << "3. Insert at ith position" << endl;
         cout << "4. Delete at ith postion" << endl;
+        cout << "5. Delete specific value" << endl;
+        cout << "6. Sort" << endl;
+        cout << "7. Reverse sort" << endl;
 
         cout << endl << "Enter your choice : ";
         cin >> choice;
@@ -116,6 +188,15 @@ main()
             break;
         case 4:
             arr.deletei();
+            break;
+        case 5:
+            arr.deletespecific();
+            break;
+        case 6:
+            arr.sort();
+            break;
+        case 7:
+            arr.rsort();
             break;
         case 100:
             cout << "Thank you !" << endl;
