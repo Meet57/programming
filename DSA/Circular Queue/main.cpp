b4e2d0f1b2ca9e30954d018090daa7f8c00bedae
Meet Patel <45942161+Meet57@users.noreply.github.com>
2019-07-09 19:58:22 +0530

    kkk

--- DSA/Circular Queue/main.cpp
+++ DSA/Circular Queue/main.cpp
@@ -1,45 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-class Queue
-{
-    int a[5],
-};
-
-main()
-{
-    Queue qu;
-    int choice=9;
-
-    while(choice!=4)
-    {
-        cout << endl << "-------Queue-------" << endl;
-        cout << "1. Enter value in Queue." << endl;
-        cout << "2. Pop value from Queue." << endl;
-        cout << "3. Display queue." << endl;
-        cout << "4. Exit." << endl;
-        cout << endl << "Enter your choice : ";
-        cin >> choice;
-
-        switch(choice)
-        {
-        case 1:
-            qu.insert();
-            break;
-        case 2:
-            qu.pop();
-            break;
-        case 3:
-            qu.display();
-            break;
-        case 4:
-            break;
-        default:
-            cout << "Invalid operation" << endl;
-            break;
-        }
-        cout << "----------------------------------------" << endl;
-    }
-    cout << "Thank you !" << endl;
+int queue[5], n = 5, front = - 1, rear = - 1;
+void Insert() {
+   int val;
+   if (rear == n - 1)
+      cout<<"Queue Overflow"<<endl;
+   else {
+      if (front == - 1)
+      front = 0;
+      cout<<"Insert the element in queue : "<<endl;
+      cin>>val;
+      rear++;
+      queue[rear] = val;
+   }
 }
-
+void Delete() {
+   if (front == - 1 || front > rear) {
+      cout<<"Queue Underflow ";
+   return ;
+   } else {
+      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
+      front++;;
+   }
+}
+void Display() {
+   if (front == - 1)
+   cout<<"Queue is empty"<<endl;
+   else {
+      cout<<"Queue elements are : ";
+      for (int i = front; i <= rear; i++)
+         cout<<queue[i]<<" ";
+      cout<<endl;
+   }
+}
+int main() {
+   int ch;
+   cout<<"1) Insert element to queue"<<endl;
+   cout<<"2) Delete element from queue"<<endl;
+   cout<<"3) Display all the elements of queue"<<endl;
+   cout<<"4) Exit"<<endl;
+do {
+   cout<<"Enter your choice : "<<endl;
+   cin>>ch;
+   switch (ch) {
+      case 1: Insert();
+         break;
+      case 2: Delete();
+         break;
+      case 3: Display();
+         break;
+      case 4: cout<<"Exit"<<endl;
+         break;
+      default: cout<<"Invalid choice"<<endl;
+   }
+} while(ch!=4);
+   return 0;
+}
