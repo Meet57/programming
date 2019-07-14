2dfb4dc444d40d980881325bd0394af6699cdc13
Meet Patel <45942161+Meet57@users.noreply.github.com>
2019-07-14 15:56:34 +0530

    Queue test

--- DSA/Circular Queue/main.cpp
+++ DSA/Circular Queue/main.cpp
@@ -1,58 +1,109 @@
 #include <iostream>
 using namespace std;
-int queue[5], n = 5, front = - 1, rear = - 1;
-void Insert() {
-   int val;
-   if (rear == n - 1)
-      cout<<"Queue Overflow"<<endl;
-   else {
-      if (front == - 1)
-      front = 0;
-      cout<<"Insert the element in queue : "<<endl;
-      cin>>val;
-      rear++;
-      queue[rear] = val;
-   }
+
+class Queue
+{
+    int a[5],n,front=(-1),rear=(-1),size=5,i;
+public:
+    void enqueue()
+    {
+        if((front==0 && rear==(size-1)) || (front == rear+1) )
+        {
+            cout << endl << "Queue Overflow !" << endl;
+            return;
+        }
+        else if(rear == (-1))
+        {
+            front++;
+            rear++;
+        }
+        else if(rear==4 && front > size)
+        {
+            rear=0;
+        }
+        else
+        {
+            rear++;
+        }
+        cout << "Enter the value you want to insert : ";
+        cin >> a[rear];
+    }
+    void dequeue()
+    {
+        if(front==(-1))
+        {
+            cout << "Queue Underflow !";
+        }
+        else if(front == rear)
+        {
+            cout << endl << a[front] << " is deleted !" << endl;
+            front = -1;
+            rear = -1;
+        }
+        else
+        {
+            cout << endl << a[front] << " is deleted !" << endl;
+            front++;
+        }
+    }
+    void display()
+    {
+        if(front>rear)
+        {
+            for(i=front;i<size;i++)
+            {
+                cout << a[i] <<endl;
+            }
+            for(i=rear;i<front;i++)
+            {
+                cout << a[i] << endl;
+            }
+        }
+        else
+        {
+            for(i=front;i<=rear;i++)
+            {
+                cout << a[i] << endl;
+            }
+        }
+    }
+};
+
+main()
+{
+    Queue qu;
+    int choice;
+    choice = 0;
+
+    while(choice!=4)
+    {
+        cout << endl << "-------Queue-------" << endl;
+        cout << "1. Enter value in Queue." << endl;
+        cout << "2. dequeue value from Queue." << endl;
+        cout << "3. Display queue." << endl;
+        cout << "4. Exit." << endl;
+        cout << endl << "Enter your choice : ";
+        cin >> choice;
+
+        switch(choice)
+        {
+        case 1:
+            qu.enqueue();
+            break;
+        case 2:
+            qu.dequeue();
+            break;
+        case 3:
+            qu.display();
+            break;
+        case 4:
+            break;
+        default:
+            cout << "Invalid operation" << endl;
+            break;
+        }
+        cout << "----------------------------------------" << endl;
+    }
+    cout << "Thank you !" << endl;
 }
-void Delete() {
-   if (front == - 1 || front > rear) {
-      cout<<"Queue Underflow ";
-   return ;
-   } else {
-      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
-      front++;;
-   }
-}
-void Display() {
-   if (front == - 1)
-   cout<<"Queue is empty"<<endl;
-   else {
-      cout<<"Queue elements are : ";
-      for (int i = front; i <= rear; i++)
-         cout<<queue[i]<<" ";
-      cout<<endl;
-   }
-}
-int main() {
-   int ch;
-   cout<<"1) Insert element to queue"<<endl;
-   cout<<"2) Delete element from queue"<<endl;
-   cout<<"3) Display all the elements of queue"<<endl;
-   cout<<"4) Exit"<<endl;
-do {
-   cout<<"Enter your choice : "<<endl;
-   cin>>ch;
-   switch (ch) {
-      case 1: Insert();
-         break;
-      case 2: Delete();
-         break;
-      case 3: Display();
-         break;
-      case 4: cout<<"Exit"<<endl;
-         break;
-      default: cout<<"Invalid choice"<<endl;
-   }
-} while(ch!=4);
-   return 0;
-}
+
