bfaab2855a7c0a999eee7fcc73baa58712ee2185
Meet Patel <45942161+Meet57@users.noreply.github.com>
2019-07-29 21:11:14 +0530

    itw 3

--- Basic Python/practice/try3.py
+++ Basic Python/practice/try3.py
@@ -1,2 +1,5 @@
-i = input("asdasd\t")
-print(i)
+price = float(input("Price of phone:"))
+GST = float(input("GST rate:"))
+pgst = price*GST/100
+
+print("\n\n\nPhone price : ",(price-pgst),"\nGST rate : ",GST,"\nGST price : ",pgst,"\nTotal Bill :",price)
