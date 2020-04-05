0961009266323c29521167dc83e1ca46a8287146
Meet Patel <45942161+Meet57@users.noreply.github.com>
2020-04-05 11:45:44 +0530

    nothing

--- Basic Python/PycharmPractice/deep.py
+++ Basic Python/PycharmPractice/deep.py
@@ -8,7 +8,6 @@ class Student:
         self.Enrollment = input("Enter Enrollment : ")
         self.spi = float(input("Enter SPI : "))
         self.cpi = float(input("Enter CPI : "))
-
     def printValues(self):
         print("Name of the Student :", self.Name, "\nEnrollment No :", self.Enrollment, "\nSPI :", self.spi, "\nCPI : ",
               self.cpi)
